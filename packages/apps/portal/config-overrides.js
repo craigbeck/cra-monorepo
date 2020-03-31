4f48169625d1daf2046f54a2ef1f5dbcb2138445
Craig Beck <luckybonza@gmail.com>
2020-03-31 14:11:04 -0700

    refactor config-overrides

--- packages/apps/portal/config-overrides.js
+++ packages/apps/portal/config-overrides.js
@@ -1,60 +1,62 @@
+const { override } = require('customize-cra');
 const path = require('path');
 
-const additionalPresets = [
-  [
-    require.resolve('babel-preset-react-app'),
-    { helpers: true, flow: false, typescript: true },
-  ],
-];
 
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  // console.log('env =', env);
-  
-  const [
-    rule0,
-    rule1,
-    rule2,
-    ...otherRules
-  ] = config.module.rules;
+const appSrc = path.join(__dirname, '../..');
 
-  const [
-    opt0,
-    target,
-    ...otherOpts
-  ] = rule2.oneOf;
-
-  // const target = config.module.rules[2].oneOf[1];
-  console.log(JSON.stringify(target, null, 2));
-
-  const newOptions = {
-    ...target,
-    include: path.resolve(target.include, '../../..'),
-    options: {
-      ...target.options,
-      presets: additionalPresets,
+const adjustLoaderForWorkspace = includePath => config => {
+  const rules = config.module.rules.map(r => {
+    // want the rule defining the loaders
+    // this is the rule with `oneOf` attibute
+    // anything else ignored  
+    if (!r.oneOf) {
+      return r;
     }
-  };
 
-  console.log(JSON.stringify(newOptions, null, 2));
+    return {
+      oneOf: r.oneOf.map(o => {
+        // ignore if not using babel-loader
+        if (!/babel-loader/.test(o.loader)) {
+          return o;
+        }
+
+        // babel-loader can be used without include path
+        // but we want the one with the defined include
+        if (!o.include) {
+          return o;
+        }
+
+        // this is the rule we want
+        return {
+          ...o,
+          // set include path to the `packages` directory so
+          // we transpile any of our dependent packages within
+          // the monorepo 
+          include: includePath,
+          options: {
+            ...o.options,
+            // adding this preset enabling typescript compilation
+            presets: [
+              [
+                require.resolve('babel-preset-react-app'),
+                { helpers: true, flow: false, typescript: true },
+              ],
+            ],
+          }
+        };
+      })
+    };
+  });
 
   return {
     ...config,
     module: {
       ...config.module,
-      rules: [
-        rule0,
-        rule1,
-        {
-          ...rule2,
-          oneOf: [
-            opt0,
-            newOptions,
-            ...otherOpts,
-          ]
-        },
-        ...otherRules,
-      ],
-    }
+      rules,
+    },
   };
 }
+
+module.exports = override(
+  adjustLoaderForWorkspace(appSrc)
+);
